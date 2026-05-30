49e23b577ccfe22cdadc9fc13238eba79333cb39
AlphaSurgeMask <265499763+AlphaSurgeMask@users.noreply.github.com>
2026-05-30 10:05:30 +0000

    Migrate textToASCII, ASCIIToBinary and XORgate functions to operate per character allowing encryptText function to control the loop process.

--- src/node/index.js
+++ src/node/index.js
@@ -4,26 +4,29 @@ let encryptionKey = 5;
 let bitLength = 8;
 
 let initializationVector = randomBinary(bitLength);
-let encryptedText;
 
 let rl = readline.createInterface(process.stdin, process.stdout);
 
 rl.question("What text do you want to encrypt? ", (plainText) => {
   console.log("Using text: " + plainText);
 
-  encryptedText = encryptText(plainText, initializationVector);
-
-  console.log("The original text in binary:");
-  console.log(ASCIIToBinary(textToASCII(plainText)));
-
-  console.log("Using the initialization vector:");
-  console.log(initializationVector);
+  console.log("\n");
 
   console.log("The encrypted text in binary:");
-  console.log(encryptedText);
+  encryptText(plainText, initializationVector);
 
   rl.close();
 });
+
+// Helper functions
+
+function numberToBinary(number, length) {
+  let binaryCode = (number >>> 0).toString(2);
+  for (let i = 0; i < length - (number >>> 0).toString(2).length; i++) {
+    binaryCode = "0" + binaryCode;
+  }
+  return binaryCode;
+}
 
 function randomBinary(length) {
   let binaryCode = "";
@@ -35,63 +38,64 @@ function randomBinary(length) {
   return binaryCode;
 }
 
-function textToASCII(text) {
-  let asciiArray = [];
+// Encryption functions
 
-  for (let i = 0; i < text.length; i++) {
-    asciiArray[i] = text.charCodeAt(i);
-  }
-
-  return asciiArray;
+function textToASCII(character) {
+  return character.charCodeAt(0);
 }
 
-function ASCIIToBinary(text) {
-  let binaryArray = [];
-
-  for (let i = 0; i < text.length; i++) {
-    binaryArray[i] = "0" + text[i].toString(2);
-  }
-
-  return binaryArray;
+function ASCIIToBinary(character) {
+  return "0" + character.toString(2);
 }
 
-function XORgate(text, IV) {
-  let XORArray = [];
+function XORgate(character, IV) {
+  let binaryString = "";
 
-  for (let i = 0; i < text.length; i++) {
-    let binaryString = "";
-
-    for (let j = 0; j < text[i].length; j++) {
-      binaryString += text[i][j] ^ IV[j];
-    }
-    XORArray.push(binaryString);
-    IV = binaryString;
+  for (let j = 0; j < character.length; j++) {
+    binaryString += character[j] ^ IV[j];
   }
 
-  return XORArray;
-}
-
-function encryptText(text, IV) {
-  return caesarCipher(XORgate(ASCIIToBinary(textToASCII(text)), IV), encryptionKey, bitLength);
-}
-
-function numberToBinary(number, length) {
-  let binaryCode = (number >>> 0).toString(2);
-  for (let i = 0; i < length - (number >>> 0).toString(2).length; i++) {
-    binaryCode = "0" + binaryCode;
-  }
-  return binaryCode;
+  return binaryString;
 }
 
 function caesarCipher(text, key, length) {
-  let cipherText = [];
-  for (let i = 0; i < text.length; i++) {
-    cipherText.push(
-      (parseInt(text[i], 2) + parseInt(numberToBinary(key, bitLength), 2)).toString(2),
-    );
-    while (cipherText[i].length < length) {
-      cipherText[i] = "0" + cipherText[i];
-    }
+  let cipherText;
+
+  cipherText = (
+    parseInt(text, 2) + parseInt(numberToBinary(key, bitLength), 2)
+  ).toString(2);
+  while (cipherText.length < length) {
+    cipherText = "0" + cipherText;
   }
   return cipherText;
+}
+
+// TODO: Solve issue with binary ticking over to 9 bits
+// TODO: Research if code is consistent over multiple hand tests
+function encryptText(text, IV) {
+  let encryptedText = text.split("");
+
+  for (let i = 0; i < encryptedText.length; i++) {
+    encryptedText.splice(i, 1, ASCIIToBinary(textToASCII(encryptedText[i])));
+
+    console.log("Text in binary:");
+    console.log(encryptedText);
+
+    if (i == 0) {
+      encryptedText.splice(i, 1, XORgate(encryptedText[i], IV));
+      console.log("Using IV: " + IV);
+    } else {
+      encryptedText.splice(i, 1, XORgate(encryptedText[i], encryptedText[i - 1]));
+      console.log("Using IV: " + encryptedText[i - 1]);
+    }
+    console.log("Text after XOR gate:");
+    console.log(encryptedText);
+
+    encryptedText.splice(i, 1, caesarCipher(encryptedText[i], encryptionKey, 8));
+
+    console.log("Text after cipher:");
+    console.log(encryptedText);
+    console.log("\n");
+  }
+  return encryptedText;
 }
