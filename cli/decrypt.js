emmauscc/block-encryption-le-jolly-fellows

commit 666f847979497857d1550d7dbef781c816a61a54

Make more code shorter, use strict equality/inequality where possible and delete old code. Bump version.

cli/decrypt.js
@@ -19,10 +19,10 @@ if (process.argv.length == 6) {
       rl.question("What was the initialization vector used? ", (initializationVector) => {
         rl.question("Finally, what cipher? (C)aesar/(V)igenere ", (cipherMethod) => {
           console.log("\n");
+          
           if (cipherMethod.toLowerCase() == "c" || cipherMethod.toLowerCase() == "caesar") {
             decryptionProcess(textToDecrypt, encryptionKey, initializationVector, "caesar");
           } else if (cipherMethod.toLowerCase() == "v" || cipherMethod.toLowerCase() == "vigenere") {
-            console.log("\n");
             console.warn("Warning! Vigenere cipher only operates on letters!");
             console.warn("Warning! Vigenere cipher will convert lower case to upper case!");
             decryptionProcess(textToDecrypt, encryptionKey, initializationVector, "vigenere");
@@ -40,21 +40,21 @@ function decryptText(text, key, IV, cipher) {
   text = text.match(/.{1,8}/g);
 
   for (let i = text.length - 1; i > -1; i--) {
-    if (cipher == "caesar") {
-      text.splice(i, 1, caesarCipher(text[i], -key));
+    if (cipher === "caesar") {
+      text[i] = caesarCipher(text[i], -key);
     }
 
     if (i == 0) {
-      text.splice(i, 1, XORgate(text[i], IV));
+      text[i] = XORgate(text[i], IV);
     } else {
-      text.splice(i, 1, XORgate(text[i], text[i - 1]));
+      text[i] = XORgate(text[i], text[i - 1]);
     }
   }
 
   for (let i = 0; i < text.length; i++) {
-    text.splice(i, 1, String.fromCharCode(parseInt(text[i], 2)));
+    text[i] = String.fromCharCode(parseInt(text[i], 2));
   }
-  if (cipher == "vigenere"){
+  if (cipher === "vigenere"){
     text = vigenereCipher(text.join(""), key, 0).split("");
   }
   return text;
@@ -66,11 +66,11 @@ function decryptionProcess(text, key, IV, cipher) {
   console.log("Using key: " + key);
   console.log("Using IV: " + IV);
 
-  if (cipher == "caesar") {
+  if (cipher === "caesar") {
     console.log("\n");
     console.log("The decrypted text in plain text:");
     console.log(decryptText(text, parseInt(key), IV, cipher).join(""));
-  } else if (cipher == "vigenere") {
+  } else if (cipher === "vigenere") {
     console.log("\n");
     console.log("The decrypted text in plain text:");
     console.log(decryptText(text, key, IV, cipher).join(""));

cli/encrypt.js
@@ -20,10 +20,10 @@ if (process.argv.length == 5) {
     rl.question("What is your encryption key? ", (encryptionKey) => {
       rl.question("What cipher do you want to use? (C)aesar/(V)igenere ", (cipherMethod) => {
           console.log("\n");
+          
           if (cipherMethod.toLowerCase() == "c" || cipherMethod.toLowerCase() == "caesar") {
             encryptionProcess(textToEncrypt, encryptionKey, initializationVector, "caesar");
           } else if (cipherMethod.toLowerCase() == "v" || cipherMethod.toLowerCase() == "vigenere") {
-            console.log("\n");
             console.warn("Warning! Vigenere cipher only operates on letters!");
             console.warn("Warning! Vigenere cipher will convert lower case to upper case!");
             encryptionProcess(textToEncrypt, encryptionKey, initializationVector, "vigenere",);
@@ -62,23 +62,23 @@ function ASCIIToBinary(character) {
 
 // TODO: Research if code is consistent over multiple hand tests
 function encryptText(text, key, IV, cipher) {
-  if (cipher == "caesar") {
+  if (cipher === "caesar") {
     text = text.split("");
-  } else if (cipher == "vigenere"){
+  } else if (cipher === "vigenere"){
     text = vigenereCipher(text.toUpperCase(), key, 1).split("");
   }
 
   for (let i = 0; i < text.length; i++) {
-    text.splice(i, 1, ASCIIToBinary(textToASCII(text[i])));
+    text[i] = ASCIIToBinary(textToASCII(text[i]));
 
     if (i == 0) {
-      text.splice(i, 1, XORgate(text[i], IV));
+      text[i] = XORgate(text[i], IV);
     } else {
-      text.splice(i, 1, XORgate(text[i], text[i - 1]));
+      text[i] = XORgate(text[i], text[i - 1]);
     }
 
-    if (cipher == "caesar") {
-      text.splice(i, 1, caesarCipher(text[i], key));
+    if (cipher === "caesar") {
+      text[i] = caesarCipher(text[i], key);
     }
   }
   return text;
@@ -90,14 +90,14 @@ function encryptionProcess(text, key, IV, cipher) {
   console.log("Using key: " + key);
   console.log("Using IV: " + IV);
 
-  if (cipher == "caesar") {
+  if (cipher === "caesar") {
     if (key > 64 || key <= 0) {
       throw new Error("Encryption key too high or negative! Please keep it lower than or equal to 64");
     }
     console.log("\n");
     console.log("The encrypted text in binary:");
     console.log(encryptText(text, parseInt(key), IV, cipher).join(""));
-  } else if (cipher == "vigenere") {
+  } else if (cipher === "vigenere") {
     console.log("\n");
     console.log("The encrypted text in binary:");
     console.log(encryptText(text, key, IV, cipher).join(""));

src/scripts/decrypt.js
@@ -4,38 +4,39 @@ function decryptText(text, key, IV, cipher) {
   text = text.match(/.{1,8}/g);
 
   for (let i = text.length - 1; i > -1; i--) {
-    if (cipher == "caesar") {
-      text.splice(i, 1, caesarCipher(text[i], -key));
+    if (cipher === "caesar") {
+      text[i] = caesarCipher(text[i], -key);
     }
 
     if (i == 0) {
-      text.splice(i, 1, XORgate(text[i], IV));
+      text[i] = XORgate(text[i], IV);
     } else {
-      text.splice(i, 1, XORgate(text[i], text[i - 1]));
+      text[i] = XORgate(text[i], text[i - 1]);
     }
   }
 
   for (let i = 0; i < text.length; i++) {
-    text.splice(i, 1, String.fromCharCode(parseInt(text[i], 2)));
+    text[i] = String.fromCharCode(parseInt(text[i], 2));
   }
-  if (cipher == "vigenere"){
+  if (cipher === "vigenere"){
     text = vigenereCipher(text.join(""), key, 0).split("");
   }
   return text;
 }
 
 function decryptionProcess(text, key, IV, cipher) {
-  document.getElementById("output").innerHTML = "Using IV: " + IV;
+  let output = document.getElementById("output");
+  output.innerHTML = "Using IV: " + IV;
 
-  if (cipher == "caesar") {
-    document.getElementById("output").innerHTML += "<br>";
+  if (cipher === "caesar") {
+    output.innerHTML += "<br>";
 
-    document.getElementById("output").innerHTML += "<br>" + "The decrypted text in plain text:";
-    document.getElementById("output").innerHTML += "<br>" + decryptText(text, parseInt(key), IV, cipher).join("");
-  } else if (cipher == "vigenere") {
-    document.getElementById("output").innerHTML += "<br>";
+    output.innerHTML += "<br>" + "The decrypted text in plain text:";
+    output.innerHTML += "<br>" + decryptText(text, parseInt(key), IV, cipher).join("");
+  } else if (cipher === "vigenere") {
+    output.innerHTML += "<br>";
     
-    document.getElementById("output").innerHTML += "<br>" + "The encrypted text in binary:";
-    document.getElementById("output").innerHTML += "<br>" + decryptText(text, key, IV, cipher).join("");
+    output.innerHTML += "<br>" + "The encrypted text in binary:";
+    output.innerHTML += "<br>" + decryptText(text, key, IV, cipher).join("");
   }
 }

src/scripts/encrypt.js
@@ -25,44 +25,45 @@ function ASCIIToBinary(character) {
 
 // TODO: Research if code is consistent over multiple hand tests
 function encryptText(text, key, IV, cipher) {
-  if (cipher == "caesar") {
+  if (cipher === "caesar") {
     text = text.split("");
-  } else if (cipher == "vigenere"){
+  } else if (cipher === "vigenere"){
     text = vigenereCipher(text.toUpperCase(), key, 1).split("");
   }
 
   for (let i = 0; i < text.length; i++) {
-    text.splice(i, 1, ASCIIToBinary(textToASCII(text[i])));
+    text[i] = ASCIIToBinary(textToASCII(text[i]));
 
     if (i == 0) {
-      text.splice(i, 1, XORgate(text[i], IV));
+      text[i] = XORgate(text[i], IV);
     } else {
-      text.splice(i, 1, XORgate(text[i], text[i - 1]));
+      text[i] = XORgate(text[i], text[i - 1]);
     }
 
-    if (cipher == "caesar") {
-      text.splice(i, 1, caesarCipher(text[i], key));
+    if (cipher === "caesar") {
+      text[i] = caesarCipher(text[i], key);
     }
   }
   return text;
 }
 
 function encryptionProcess(text, key, IV, cipher) {
-  document.getElementById("output").innerHTML = "Using IV: " + IV;
+  let output = document.getElementById("output");
+  output.innerHTML = "Using IV: " + IV;
 
-  if (cipher == "caesar") {
+  if (cipher === "caesar") {
     if (key > 64 || key <= 0) {
-      document.getElementById("output").innerHTML = "<br>" + "Encryption key too high or negative! Please keep it lower than or equal to 64";
+      output.innerHTML = "<br>" + "Encryption key too high or negative! Please keep it lower than or equal to 64";
       return;
     }
-    document.getElementById("output").innerHTML += "<br>";
+    output.innerHTML += "<br>";
 
-    document.getElementById("output").innerHTML += "<br>" + "The encrypted text in binary:";
-    document.getElementById("output").innerHTML += "<br>" + encryptText(text, parseInt(key), IV, cipher).join("");
-  } else if (cipher == "vigenere") {
-    document.getElementById("output").innerHTML += "<br>";
+    output.innerHTML += "<br>" + "The encrypted text in binary:";
+    output.innerHTML += "<br>" + encryptText(text, parseInt(key), IV, cipher).join("");
+  } else if (cipher === "vigenere") {
+    output.innerHTML += "<br>";
     
-    document.getElementById("output").innerHTML += "<br>" + "The encrypted text in binary:";
-    document.getElementById("output").innerHTML += "<br>" + encryptText(text, key, IV, cipher).join("");
+    output.innerHTML += "<br>" + "The encrypted text in binary:";
+    output.innerHTML += "<br>" + encryptText(text, key, IV, cipher).join("");
   }
 }

src/scripts/main.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 function displayCipherDropdown() {
-  if (document.getElementById("text-input").value != "") {
+  if (document.getElementById("text-input").value !== "") {
     document.getElementById("cipherDropdown").style.display = "inline-block";
     document.getElementById("key-input").style.display = "inline-block";
   } else {
@@ -11,7 +11,7 @@ function displayCipherDropdown() {
 }
 
 function displayModeDropdown() {
-  if (document.getElementById("key-input").value != "") {
+  if (document.getElementById("key-input").value !== "") {
     document.getElementById("modeDropdown").style.display = "inline-block";
     document.getElementById("runButton").style.display = "inline-block";
   } else {

src-tauri/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "block-encryptor"
-version = "1.1.0"
+version = "1.2.0"
 description = "A simple project to implement block encryption with JavaScript"
 authors = ["JM and ID"]
 edition = "2021"

src/styles.css
@@ -84,13 +84,6 @@ button:active {
   background-color: #e8e8e8;
 }
 
-#output,
-input,
-select,
-button {
-  outline: none;
-}
-
 #greet-input {
   margin-right: 5px;
 }
@@ -116,7 +109,7 @@ button {
 #cipherDropdown,
 #key-input,
 #modeDropdown,
-#iv-input,i
+#iv-input,
 #runButton {
   display: none;
 }

commit 4c0712c8c685e6f346561eff9010e4c65a0115fe

Completely revert javascript changes in commit 666f847, caused issues.

cli/decrypt.js
@@ -19,7 +19,7 @@ if (process.argv.length == 6) {
       rl.question("What was the initialization vector used? ", (initializationVector) => {
         rl.question("Finally, what cipher? (C)aesar/(V)igenere ", (cipherMethod) => {
           console.log("\n");
-          
+
           if (cipherMethod.toLowerCase() == "c" || cipherMethod.toLowerCase() == "caesar") {
             decryptionProcess(textToDecrypt, encryptionKey, initializationVector, "caesar");
           } else if (cipherMethod.toLowerCase() == "v" || cipherMethod.toLowerCase() == "vigenere") {
@@ -40,7 +40,7 @@ function decryptText(text, key, IV, cipher) {
   text = text.match(/.{1,8}/g);
 
   for (let i = text.length - 1; i > -1; i--) {
-    if (cipher === "caesar") {
+    if (cipher == "caesar") {
       text.splice(i, 1, caesarCipher(text[i], -key));
     }
 
@@ -54,7 +54,7 @@ function decryptText(text, key, IV, cipher) {
   for (let i = 0; i < text.length; i++) {
     text.splice(i, 1, String.fromCharCode(parseInt(text[i], 2)));
   }
-  if (cipher === "vigenere"){
+  if (cipher == "vigenere"){
     text = vigenereCipher(text.join(""), key, 0).split("");
   }
   return text;
@@ -66,11 +66,11 @@ function decryptionProcess(text, key, IV, cipher) {
   console.log("Using key: " + key);
   console.log("Using IV: " + IV);
 
-  if (cipher === "caesar") {
+  if (cipher == "caesar") {
     console.log("\n");
     console.log("The decrypted text in plain text:");
     console.log(decryptText(text, parseInt(key), IV, cipher).join(""));
-  } else if (cipher === "vigenere") {
+  } else if (cipher == "vigenere") {
     console.log("\n");
     console.log("The decrypted text in plain text:");
     console.log(decryptText(text, key, IV, cipher).join(""));

cli/encrypt.js
@@ -62,9 +62,9 @@ function ASCIIToBinary(character) {
 
 // TODO: Research if code is consistent over multiple hand tests
 function encryptText(text, key, IV, cipher) {
-  if (cipher === "caesar") {
+  if (cipher == "caesar") {
     text = text.split("");
-  } else if (cipher === "vigenere"){
+  } else if (cipher == "vigenere"){
     text = vigenereCipher(text.toUpperCase(), key, 1).split("");
   }
 
@@ -77,7 +77,7 @@ function encryptText(text, key, IV, cipher) {
       text.splice(i, 1, XORgate(text[i], text[i - 1]));
     }
 
-    if (cipher === "caesar") {
+    if (cipher == "caesar") {
       text.splice(i, 1, caesarCipher(text[i], key));
     }
   }
@@ -90,14 +90,14 @@ function encryptionProcess(text, key, IV, cipher) {
   console.log("Using key: " + key);
   console.log("Using IV: " + IV);
 
-  if (cipher === "caesar") {
+  if (cipher == "caesar") {
     if (key > 64 || key <= 0) {
       throw new Error("Encryption key too high or negative! Please keep it lower than or equal to 64");
     }
     console.log("\n");
     console.log("The encrypted text in binary:");
     console.log(encryptText(text, parseInt(key), IV, cipher).join(""));
-  } else if (cipher === "vigenere") {
+  } else if (cipher == "vigenere") {
     console.log("\n");
     console.log("The encrypted text in binary:");
     console.log(encryptText(text, key, IV, cipher).join(""));

src/scripts/decrypt.js
@@ -4,8 +4,8 @@ function decryptText(text, key, IV, cipher) {
   text = text.match(/.{1,8}/g);
 
   for (let i = text.length - 1; i > -1; i--) {
-    if (cipher === "caesar") {
-      text.splice(i, 1, caesarCipher(text[i], -key))
+    if (cipher == "caesar") {
+      text.splice(i, 1, caesarCipher(text[i], -key));
     }
 
     if (i == 0) {
@@ -18,25 +18,24 @@ function decryptText(text, key, IV, cipher) {
   for (let i = 0; i < text.length; i++) {
     text.splice(i, 1, String.fromCharCode(parseInt(text[i], 2)));
   }
-  if (cipher === "vigenere"){
+  if (cipher == "vigenere"){
     text = vigenereCipher(text.join(""), key, 0).split("");
   }
   return text;
 }
 
 function decryptionProcess(text, key, IV, cipher) {
-  let output = document.getElementById("output");
-  output.innerHTML = "Using IV: " + IV;
+  document.getElementById("output").innerHTML = "Using IV: " + IV;
 
-  if (cipher === "caesar") {
-    output.innerHTML += "<br>";
+  if (cipher == "caesar") {
+    document.getElementById("output").innerHTML += "<br>";
 
-    output.innerHTML += "<br>" + "The decrypted text in plain text:";
-    output.innerHTML += "<br>" + decryptText(text, parseInt(key), IV, cipher).join("");
-  } else if (cipher === "vigenere") {
-    output.innerHTML += "<br>";
+    document.getElementById("output").innerHTML += "<br>" + "The decrypted text in plain text:";
+    document.getElementById("output").innerHTML += "<br>" + decryptText(text, parseInt(key), IV, cipher).join("");
+  } else if (cipher == "vigenere") {
+    document.getElementById("output").innerHTML += "<br>";
     
-    output.innerHTML += "<br>" + "The encrypted text in binary:";
-    output.innerHTML += "<br>" + decryptText(text, key, IV, cipher).join("");
+    document.getElementById("output").innerHTML += "<br>" + "The encrypted text in binary:";
+    document.getElementById("output").innerHTML += "<br>" + decryptText(text, key, IV, cipher).join("");
   }
 }

src/scripts/encrypt.js
@@ -25,9 +25,9 @@ function ASCIIToBinary(character) {
 
 // TODO: Research if code is consistent over multiple hand tests
 function encryptText(text, key, IV, cipher) {
-  if (cipher === "caesar") {
+  if (cipher == "caesar") {
     text = text.split("");
-  } else if (cipher === "vigenere"){
+  } else if (cipher == "vigenere"){
     text = vigenereCipher(text.toUpperCase(), key, 1).split("");
   }
 
@@ -40,7 +40,7 @@ function encryptText(text, key, IV, cipher) {
       text.splice(i, 1, XORgate(text[i], text[i - 1]));
     }
 
-    if (cipher === "caesar") {
+    if (cipher == "caesar") {
       text.splice(i, 1, caesarCipher(text[i], key));
     }
   }
@@ -48,22 +48,21 @@ function encryptText(text, key, IV, cipher) {
 }
 
 function encryptionProcess(text, key, IV, cipher) {
-  let output = document.getElementById("output");
-  output.innerHTML = "Using IV: " + IV;
+  document.getElementById("output").innerHTML = "Using IV: " + IV;
 
-  if (cipher === "caesar") {
+  if (cipher == "caesar") {
     if (key > 64 || key <= 0) {
-      output.innerHTML = "<br>" + "Encryption key too high or negative! Please keep it lower than or equal to 64";
+      document.getElementById("output").innerHTML = "<br>" + "Encryption key too high or negative! Please keep it lower than or equal to 64";
       return;
     }
-    output.innerHTML += "<br>";
+    document.getElementById("output").innerHTML += "<br>";
 
-    output.innerHTML += "<br>" + "The encrypted text in binary:";
-    output.innerHTML += "<br>" + encryptText(text, parseInt(key), IV, cipher).join("");
-  } else if (cipher === "vigenere") {
-    output.innerHTML += "<br>";
+    document.getElementById("output").innerHTML += "<br>" + "The encrypted text in binary:";
+    document.getElementById("output").innerHTML += "<br>" + encryptText(text, parseInt(key), IV, cipher).join("");
+  } else if (cipher == "vigenere") {
+    document.getElementById("output").innerHTML += "<br>";
     
-    output.innerHTML += "<br>" + "The encrypted text in binary:";
-    output.innerHTML += "<br>" + encryptText(text, key, IV, cipher).join("");
+    document.getElementById("output").innerHTML += "<br>" + "The encrypted text in binary:";
+    document.getElementById("output").innerHTML += "<br>" + encryptText(text, key, IV, cipher).join("");
   }
 }

src/scripts/main.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 function displayCipherDropdown() {
-  if (document.getElementById("text-input").value !== "") {
+  if (document.getElementById("text-input").value != "") {
     document.getElementById("cipherDropdown").style.display = "inline-block";
     document.getElementById("key-input").style.display = "inline-block";
   } else {
@@ -11,7 +11,7 @@ function displayCipherDropdown() {
 }
 
 function displayModeDropdown() {
-  if (document.getElementById("key-input").value !== "") {
+  if (document.getElementById("key-input").value != "") {
     document.getElementById("modeDropdown").style.display = "inline-block";
     document.getElementById("runButton").style.display = "inline-block";
   } else {